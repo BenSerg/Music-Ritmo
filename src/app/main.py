78eca7a190867e211021b281e900633af793b6b6
Batueva Vlada <irelmtt@gmail.com>
2025-01-20 19:39:13 +0300

    [mus-105] Add login endpoint

--- src/app/main.py
+++ src/app/main.py
@@ -3,6 +3,7 @@ from .database import init_db
 from .db_endpoints import router
 from .open_subsonic_api import open_subsonic_router
 from .generate_avatar import generate_avatar_router
+from .login import login_router
 
 app = FastAPI()
 
@@ -13,3 +14,5 @@ app.include_router(router)
 app.include_router(open_subsonic_router)
 
 app.include_router(generate_avatar_router)
+
+app.include_router(login_router)
