3859fb54781216db20c9a24afedd28b038f73f4f
Yaroslav Mazin <ya.bigmazzy@gmail.com>
2025-01-21 17:26:41 +0300

    Add tests for opensubsonic search

--- tests/test_open_subsonic_api.py
+++ tests/test_open_subsonic_api.py
@@ -1,3 +1,4 @@
+import json
 from src.app.main import app
 from fastapi.testclient import TestClient
 
@@ -14,3 +15,21 @@ def test_ping():
     assert rsp["openSubsonic"] == True
     assert rsp["version"] is not None
     assert rsp["serverVersion"] is not None
+
+def test_search():
+    query = "track"
+    response = client.post("/rest/search", json={"query": query})
+    assert response.status_code == 200
+
+    rsp = response.json()["subsonic-response"]
+    assert rsp["status"] == "ok"
+    assert rsp["type"] == "MusicRitmo"
+    assert rsp["openSubsonic"] == True
+    assert rsp["version"] is not None
+    assert rsp["serverVersion"] is not None
+    assert rsp["searchResult"] is not None
+    for _, v in rsp["searchResult"].items():
+        print(v)
+        for i in v:
+            res = json.loads(i)
+            assert query in res["name"]
